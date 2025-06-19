0aff63c803b74a83350b6954fdda58fbc293f099
ramazanismayilov <ramazanismayilovh@gmail.com>
2025-06-19 10:08:53 +0400

    fix: add root GET route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
 
 @Controller()
-export class AppController {}
+export class AppController {
+    @Get()
+  getRoot(): string {
+    return 'NestJS API is running!';
+  }
+}
